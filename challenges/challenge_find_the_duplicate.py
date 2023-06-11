6051d3a32e960148ed7470c7822a87ec1d67b4fb
Cláudio <claudio19a@hotmail.com>
2023-06-11 20:11:12 -0300

    requisito 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,21 @@
+def is_num(nums):
+    try:
+        if any(n < 0 for n in nums):
+            return False
+    except TypeError:
+        return False
+
+    return True
+
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not is_num(nums):
+        return False
+
+    nums_set = set()
+    for num in nums:
+        if num in nums_set:
+            return num
+        nums_set.add(num)
+
+    return False
